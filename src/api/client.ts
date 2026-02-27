5bafec59694f056666cae110d205185e769b49e2
Same3rAijaz <aijazsameer5@gmail.com>
2026-02-28 04:19:41 +0500

    build udpated

--- src/api/client.ts
+++ src/api/client.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:4000/api"
+  baseURL: import.meta.env.VITE_API_URL || "http://44.200.245.10/api"
 });
 
 api.interceptors.request.use((config) => {
